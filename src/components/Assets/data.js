bf21ce65f16f19ebcf0d16eda6868f5469c4e582
Deepak <deepakjack.dj@gmail.com>
2024-06-12 16:10:02 +0530

    first commit

---
+++ src/components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+
+let data_product = [
+  {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p1_img,
+    new_price: 300,
+    old_price: 450,
+  },
+  {
+    id: 2,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p2_img,
+    new_price: 110,
+    old_price: 230,
+  },
+  {
+    id: 3,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p3_img,
+    new_price: 80,
+    old_price: 160,
+  },
+  {
+    id: 4,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p4_img,
+    new_price: 200,
+    old_price: 380,
+  },
+];
+
+export default data_product;
